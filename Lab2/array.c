571e00d363e2746a02eb527f57e21e7efa2dbaf8
JUNSOOCHO <j082175@naver.com>
2022-05-12 17:09:49 +0900

    11

--- Lab2/array.c
+++ Lab2/array.c
@@ -1,23 +1,144 @@
 #include "array.h"
+#include <stdio.h>
 
 int get_index_of(const int numbers[], const size_t element_count, const int num)
 {
-    
+	unsigned int i;
+	for (i = 0; i < element_count; i++) {
+		if (numbers[i] == num) {
+			return i;
+		}
+	}
+
+	return -1;
 }
 
 int get_last_index_of(const int numbers[], const size_t element_count, const int num)
 {
+	int i;
+	for (i = (int)element_count - 1; i >= 0; i--) {
+		if (numbers[i] == num) {
+			return i;
+		}
+	}
+
+	return -1;
 
 }
 
-int get_max_index(const int numbers[], const size_t element_count);
+int get_max_index(const int numbers[], const size_t element_count)
+{
+	if (element_count == 0) {
+		return -1;
+	}
 
-int get_min_index(const int numbers[], const size_t element_count);
+	{
+		int max_number = 0;
+		int compare_number;
+		size_t i;
+		compare_number = numbers[0];
+		for (i = 0; i < element_count; i++) {
+			if (compare_number > numbers[i]) {
+				max_number = compare_number;
+				compare_number = max_number;
+			}
+		}
 
-int is_all_positive(const int numbers[], const size_t element_count);
+		for (i = 0; i < element_count; i++) {
+			if (numbers[i] == max_number) {
+				return i;
+			}
+		}
+	}
 
-int has_even(const int numbers[], const size_t element_count);
+	return -1;
+}
 
-int insert(int numbers[], const size_t element_count, const int num, const size_t pos);
+int get_min_index(const int numbers[], const size_t element_count)
+{
+	if (element_count == 0) {
+		return -1;
+	}
 
-int remove_at(int numbers[], const size_t element_count, const size_t index);
+	{
+		int min_number = 0;
+		int compare_number = 0;
+		size_t i;
+		for (i = 0; i < element_count; i++) {
+			if (compare_number < numbers[i]) {
+				min_number = compare_number;
+				compare_number = min_number;
+			}
+		}
+
+		for (i = 0; i < element_count; i++) {
+			if (numbers[i] == min_number) {
+				return i;
+			}
+		}
+	}
+
+	return -1;
+}
+
+int is_all_positive(const int numbers[], const size_t element_count)
+{
+	int is_positive = 0;
+	size_t i;
+	for (i = 0; i < element_count; i++) {
+		if (numbers[i] > 0) {
+			is_positive = 1;
+		}
+		else {
+			is_positive = 0;
+			break;
+		}
+	}
+
+	return is_positive;
+}
+
+int has_even(const int numbers[], const size_t element_count)
+{
+	int even = 0;
+	size_t i;
+
+	for (i = 0; i < element_count; i++) {
+		if (numbers[i] % 2 == 0) {
+			even = 1;
+		}
+		else {
+			even = 0;
+			break;
+		}
+	}
+	return even;
+}
+
+int insert(int numbers[], const size_t element_count, const int num, const size_t pos)
+{
+	size_t i;
+
+	if (element_count <= pos) {
+		return 0;
+	}
+
+	for (i = element_count; i > pos; i--) {
+		numbers[i] = numbers[i - 1];
+	}
+
+	numbers[pos] = num;
+
+	return 1;
+}
+
+int remove_at(int numbers[], const size_t element_count, const size_t index)
+{
+	size_t i;
+
+	for (i = index; i < element_count - 1; i++) {
+		numbers[i] = numbers[i + 1];
+	}
+
+	return 1;
+}
