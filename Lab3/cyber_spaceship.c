0f4f1fb1226cae11e736c7fe05afe35cf8c0df88
JUNSOOCHO <j082175@naver.com>
2022-05-20 13:13:29 +0900

    1

--- Lab3/cyber_spaceship.c
+++ Lab3/cyber_spaceship.c
@@ -1,6 +1,6 @@
 #include "cyber_spaceship.h"
 
-const char *get_longest_safe_zone_or_null(const char* const cab_start_location, const size_t cab_length, const char* const cluster_start_locations[], const size_t cluster_lengths[], const size_t cluster_count, size_t* out_longest_safe_area_length)
+const char* get_longest_safe_zone_or_null(const char* const cab_start_location, const size_t cab_length, const char* const cluster_start_locations[], const size_t cluster_lengths[], const size_t cluster_count, size_t* out_longest_safe_area_length)
 {
 
     int total_length = 0;
