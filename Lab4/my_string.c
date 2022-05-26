4a285be75f0856ae3a1021f0469cf53a7dae1639
j082175 <j082175@naver.com>
2022-05-26 15:14:44 +0900

    1

--- Lab4/my_string.c
+++ Lab4/my_string.c
@@ -132,8 +132,7 @@ char* tokenize(char* str_or_null, const char* delims)
         int j;
 
         for (j = 0; j < delims_length; j++) {
-            do
-            {
+            do {
                 for (i = 0; i < delims_length; i++) {
                     if (*(str_or_null) == *(delims + i)) {
                         str_or_null = str_or_null + 1;
