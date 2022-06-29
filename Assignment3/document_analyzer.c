a22bd7a1a135f5bc9d0efb918a1025adacd95df0
JUNSOOCHO <j082175@naver.com>
2022-06-30 00:52:06 +0900

    first project

--- Assignment3/document_analyzer.c
+++ Assignment3/document_analyzer.c
@@ -1,12 +1,34 @@
 #include "document_analyzer.h"
+/* 단락 : 개행문자로 구분
+   문장 : . ? ! 3개의 문자로 구분
+   단어 : 띄어쓰기 , 문자로 구분 
+   단락과 단락사이엔 개행이 존재 
+   각각의 간격은 4개 */
 static char data[512] = { 0, };
+static char data_backup[128][32] = {0,};
 static char* recent_document = NULL;
-char**** document = data;
 
-typedef struct data1 {
-	char* arr;
-	data1_t d;
-}data1_t;
+static int total_paragraph_count = 0;
+static int total_sentence_count[128] = { 0, };
+static int total_word_count[512] = { 0, };
+
+static int total_sentence_count_int = 0;
+
+static int is_paragraph;
+static int is_word;
+static int check;
+
+static int word_count;
+
+char paragraph_division = '\n';
+char sentence_division[] = {'.', '?', '!'};
+char word_division[] = { ' ', ',' , '.', '?', '!' };
+
+char**** doc;
+char*** paragraph = NULL;
+char** sentence;
+char* word;
+
 
 int load_document(const char* document)
 {
@@ -18,15 +40,110 @@ int load_document(const char* document)
 		return FALSE;
 	}
 
+	/* 총 단락 개수 구하기 */
 	for (int i = 0; i < 512; i++)
 	{
 		data[i] = fgetc(fp);
+
 		if (data[i] == EOF)
 		{
+			total_paragraph_count++;
 			data[i] = '\0';
 			break;
 		}
+
+		if (data[i] == paragraph_division && !is_paragraph)
+		{
+			total_paragraph_count++;
+			is_paragraph = TRUE;
+		}
+
+		if (data[i] != paragraph_division)
+		{
+			is_paragraph = FALSE;
+		}
+
+		{
+			int j;
+			for (j = 0; j < 3; j++)
+			{
+				if (data[i] == sentence_division[j])
+				{
+					total_sentence_count[total_paragraph_count]++;
+					total_sentence_count_int++;
+					check = TRUE;
+					//word_count++;
+				}
+			}
+		}
+
+		{
+			int j;
+			int k;
+			for (j = 0; j < 5; j++)
+			{
+				if (data[i] == word_division[j] && !is_word)
+				{
+					//total_word_count++;
+					total_word_count[word_count]++;
+					if (check)
+					{
+						word_count++;
+						check = FALSE;
+					}
+					is_word = TRUE;
+					break;
+				}
+				else {
+					is_word = FALSE;
+				}
+			}
+		}
 	}
+
+	/* word tokenizer */
+	{
+		int count = 0;
+		char* ptr = data;
+		strtok(ptr, " ,.!?");
+		strcpy(data_backup[count++], ptr);
+		while (TRUE)
+		{
+			ptr = strtok(NULL, " ,.!?");
+			if (ptr == NULL)
+			{
+				break;
+			}
+			strcpy(data_backup[count++], ptr);
+		}
+	}
+
+	paragraph = (char***)malloc(sizeof(char**) * total_paragraph_count);
+	{
+		int i;
+		for (i = 0; i < total_paragraph_count; i++)
+		{
+			paragraph[i] = (char**)malloc(sizeof(char*) * total_sentence_count[i]);
+			printf("%d\n", _msize(paragraph[i]));
+		}
+	}
+
+	{
+		int i;
+		int j;
+		int count = 0;
+		for (i = 0; i < total_sentence_count_int; i++)
+		{
+			for (j = 0; j < total_word_count[i]; j++)
+			{
+				paragraph[i][j] = (char*)malloc(sizeof(char) * strlen(data_backup[count]) + 1);
+				strcpy(paragraph[i][j], data_backup[count]);
+				printf("%s\n", paragraph[i][j]);
+				count++;
+			}
+		}
+	}
+
 
 	if (fclose(fp) == EOF)
 	{
@@ -80,6 +197,7 @@ unsigned int get_paragraph_sentence_count(const char*** paragraph)
 const char** get_sentence_or_null(const unsigned int paragraph_index, const unsigned int sentence_index)
 {
 
+	
 }
 
 unsigned int get_sentence_word_count(const char** sentence)
--- Assignment3/main.c
+++ Assignment3/main.c
@@ -4,12 +4,46 @@
 
 /*문서는 올바른 문법과 구두점을 사용합니다
 문단들은 최소한 하나의 새 줄 문자(\n)로 구분되어 있습니다.
-문장은 ., !, ? 중의 한 문자로 구분되어 있습니다
+문장은 .!? 중의 한 문자로 구분되어 있습니다
 단어들은 빈칸(스페이스)이나 , 문자로 구분되어 있습니다.*/
 
 int main()
 {
-    const char* filepath = "text1.txt";
+	char*** paragraph = NULL;
+	int total_paragraph_count = 3;
+	int total_word_count[] = {6, 3, 4, 6, 5, 9, 3, 8, 14};
+	int total_sentence_count_int = 9;
+	int total_sentence_count[] = { 4, 2, 3 };
+	char data_backup[512];
+	strcpy(data_backup, "Let's make a simple sentence here. My second sentence. Is this too easy? Should I make the assignments harder?\n\nThis is the second paragraph.I wonder how many sentences you will see here ?\n\nPOCU is fun!Stick around and you will have MORE fun.Just one more assignment after this one, and that one will be fun too!");
+
+	paragraph = (char***)malloc(sizeof(char**) * total_paragraph_count);
+	{
+		int i;
+		for (i = 0; i < total_paragraph_count; i++)
+		{
+			paragraph[i] = (char**)malloc(sizeof(char*) * total_sentence_count[i]);
+			printf("%d\n", _msize(paragraph[i]));
+		}
+	}
+
+	{
+		int i;
+		int j;
+		int count = 0;
+		for (i = 0; i < total_sentence_count_int; i++)
+		{
+			for (j = 0; j < total_word_count[i]; j++)
+			{
+				paragraph[i][j] = (char*)malloc(sizeof(char) * strlen(data_backup[count]) + 1);
+				strcpy(paragraph[i][j], data_backup[count]);
+				printf("%s\n", paragraph[i][j]);
+				count++;
+			}
+		}
+	}
+
+    const char* filepath = "input.txt";
     load_document(filepath);
     int i = get_total_word_count();
 }
