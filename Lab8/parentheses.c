fd42a4c6563b73ebacf482b9a0e083ed42159070
JUNSOOCHO <j082175@naver.com>
2022-07-08 21:28:11 +0900

    lab_8

--- Lab8/parentheses.c
+++ Lab8/parentheses.c
@@ -1,13 +1,49 @@
 #include "parentheses.h"
 
-/* { } */
-/* ( ) */
-/* [ ] */
-/* < > */
+int MAX;
+
+int front = -1;
+int rear = -1;
+int* queue;
+
+int IsEmpty(void) {
+	if (front == rear)//front와 rear가 같으면 큐는 비어있는 상태 
+		return TRUE;
+	else return FALSE;
+}
+int IsFull(void) {
+	int tmp = (rear + 1) % MAX; //원형 큐에서 rear+1을 MAX로 나눈 나머지값이
+	if (tmp == front)//front와 같으면 큐는 가득차 있는 상태 
+		return TRUE;
+	else
+		return FALSE;
+}
+void addq(int value) {
+	if (IsFull())
+		printf("Queue is Full.\n");
+	else {
+		rear = (rear + 1) % MAX;
+		queue[rear] = value;
+	}
+
+}
+int deleteq() {
+	if (IsEmpty())
+		printf("Queue is Empty.\n");
+	else {
+		front = (front + 1) % MAX;
+		return queue[front];
+	}
+}
+
+
+
 size_t find_matching_parentheses(ringbuffer_t* ringbuffer, const char* str)
 {
 	const char outter[] = { '}', ')', ']', '>' };
 	const char inner[] = { '{',  '(',  '[',  '<' };
+	int* arr1;
+	int* arr2;
 
 	const size_t MAX_SIZE = ringbuffer->max_size;
 	const size_t STR_LENGTH = strlen(str);
@@ -19,14 +55,8 @@ size_t find_matching_parentheses(ringbuffer_t* ringbuffer, const char* str)
 		return 0;
 	}
 
-	{
-		int i;
-		for (i = 0; i < MAX_SIZE; i++)
-		{
-			ringbuffer->parentheses[i].opening_index = 0;
-			ringbuffer->parentheses[i].closing_index = 0;
-		}
-	}
+	arr1 = (int*)malloc(sizeof(int) * STR_LENGTH);
+	arr2 = (int*)malloc(sizeof(int) * STR_LENGTH);
 
 
 	strcpy(str_buf, str);
@@ -46,8 +76,10 @@ size_t find_matching_parentheses(ringbuffer_t* ringbuffer, const char* str)
 							if (str_buf[k] == inner[j]) {
 								str_buf[k] = '?';
 								str_buf[i] = '?';
-								ringbuffer->parentheses[count % MAX_SIZE].opening_index = k;
-								ringbuffer->parentheses[count % MAX_SIZE].closing_index = i;
+
+								arr1[count] = k;
+								arr2[count] = i;
+
 								if (count / MAX_SIZE) {
 									ringbuffer->start_index++;
 								}
@@ -64,6 +96,19 @@ size_t find_matching_parentheses(ringbuffer_t* ringbuffer, const char* str)
 	end:
 		count++;
 	}
+
+	{
+		int i;
+		for (i = 0; i < MAX_SIZE; i++)
+		{
+			ringbuffer->parentheses[i].opening_index = arr1[i + ringbuffer->start_index];
+			ringbuffer->parentheses[i].closing_index = arr2[i + ringbuffer->start_index];
+		}
+	}
+
+
+	free(arr1);
+	free(arr2);
 
 	if (str_buf != NULL) {
 		free(str_buf);
