228df5f97fbff0b2943ed5fdc6608dc4891e07df
j082175 <j082175@naver.com>
2022-07-08 13:58:07 +0900

    3248

--- Assignment3/document_analyzer.c
+++ Assignment3/document_analyzer.c
@@ -165,14 +165,15 @@ int load_document(const char* document)
         int count = 0;
         char* ptr;
         char* a;
-        char* data_backup1 = (char*)malloc(sizeof(char) * 512);/* 메모리 문제 1*/
-        if (data_backup1 == NULL)
-        {
-            assert(FALSE);
-        }
-        memset(data_backup1, 0, 512);
-        strcpy(data_backup1, data);
-        ptr = data_backup1;
+        //char* data_backup1 = (char*)malloc(sizeof(char) * 512);/* 메모리 문제 1*/
+        //if (data_backup1 == NULL)
+        //{
+        //    assert(FALSE);
+        //}
+        //memset(data_backup1, 0, 512);
+        //strcpy(data_backup1, data);
+        //ptr = data_backup1;
+        ptr = data;
         a = strtok(ptr, " ,.!?\n");
         if (a == NULL)
         {
@@ -189,7 +190,7 @@ int load_document(const char* document)
             strcpy(data_backup[count++], ptr);
         }
 
-        free(data_backup1);
+        //free(data_backup1);
     }
     /**/
 
