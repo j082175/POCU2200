0a18a661e5d07832cb7686fbc902a01c01817cee
j082175 <j082175@naver.com>
2022-07-11 16:56:21 +0900

    lab9_first

---
+++ Lab9/macros.h
@@ -0,0 +1,24 @@
+#ifndef MACROS_H
+#define MACROS_H
+
+#define MAX(x, y) (x > y ? x : y)
+
+#define MIN(x, y) (x < y ? x : y)
+
+#define ABS(x) (x > 0 ? x : x * -1)
+
+#define RANGE(curr, min, max)						\
+		for (curr = min; curr <= max; curr++)		\
+
+#define RANGE_DESC(curr, max, min)					\
+		for (curr = max; curr >= min; curr--)		\
+
+#define SET(ary, start, count, value)				\
+	{												\
+		int i;										\
+		for (i = start; i < count + start; i++) {	\
+			ary[i] = value;							\
+		}											\
+	}												\
+
+#endif /* MACROS_H */
