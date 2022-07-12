5b9993f88709dce9794531207ba8871a7f48ace0
JUNSOOCHO <j082175@naver.com>
2022-07-12 15:21:08 +0900

    1

--- Lab9/macros.h
+++ Lab9/macros.h
@@ -1,44 +1,29 @@
 #ifndef MACROS_H
 #define MACROS_H
 
-
-
-
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
-
-
 
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
-
 #define ABS(x) (((x) < 0) ? (-(x)) : ((x)))
 
-
 #define RANGE(curr, min, max)\
-        for (((curr) = (min)); ((curr) <= (max)); (curr++))\
+        for (((curr) = min); ((curr) <= max); (curr++))\
 
 
 #define RANGE_DESC(curr, max, min)\
-        for (((curr) = (max)); ((curr) >= (min)); (curr--))\
+        for (((curr) = max); ((curr) >= min); (curr--))\
 
-
-//#define SET(ary, start, count, value)\
-//    {\
-//        unsigned int;\
-//        for (i = (start); i < ((count) + (start)); i++) {\
-//            (ary[i]) = (value);\
-//        }\
-//    }\
 
 #define SET(ary, start, count, value)\
-do\
-{\
+        do\
         {\
-        unsigned i;\
-        for (i = (start); i < ((count) + (start)); i++) {\
-            (ary[i]) = (value);\
-        }\
-    }\
-} while (0)\
+            {\
+                unsigned i;\
+                for (i = (start); i < ((count) + (start)); i++) {\
+                (ary[i]) = (value);\
+            }\
+            }\
+        } while (0)\
 
 #endif /* MACROS_H */
