73b6a564f01c3b224fd4aa16f1b6b11bd2959c1a
JUNSOOCHO <j082175@naver.com>
2022-07-13 15:20:31 +0900

    1

--- Lab9/macros.h
+++ Lab9/macros.h
@@ -7,23 +7,23 @@
 
 #define ABS(x) (((x) < 0) ? (-(x)) : ((x)))
 
-//#define RANGE(curr, min, max)\
-//        for (((curr) = (min)); ((curr) <= (max)); (curr++))\
-//
-//
-//#define RANGE_DESC(curr, max, min)\
-//        for (((curr) = (max)); ((curr) >= (min)); (curr--))\
-
 #define RANGE(curr, min, max)\
-        (curr) = (min);\
-        (curr--);\
-        while ((curr++) < (max))\
+        for (((curr) = (min)); ((curr) <= (max)); (curr) = (curr) + 1)\
 
 
 #define RANGE_DESC(curr, max, min)\
-        (curr) = (max);\
-        (curr++);\
-        while ((curr--) > (min))\
+        for (((curr) = (max)); ((curr) >= (min)); (curr) = (curr) - 1)\
+
+//#define RANGE(curr, min, max)\
+//        (curr) = (min);\
+//        (curr--);\
+//        while ((curr++) < (max))\
+//
+//
+//#define RANGE_DESC(curr, max, min)\
+//        (curr) = (max);\
+//        (curr++);\
+//        while ((curr--) > (min))\
 
 
 #define SET(ary, start, count, value)\
